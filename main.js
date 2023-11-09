d9fc7575f278e5bfe3ad502fe726a96dd3fe4d2a
Gabriel Felix <gabriel_arturo01@hotmail.com>
2023-11-09 11:58:31 -0400

    Changed the background color of the h1 element from white to a shade of black with 30% opacity.

--- main.js
+++ main.js
@@ -1,42 +1,67 @@
-function searchPokemon() {
+async function searchPokemon() {
     const pokemonName = document.getElementById('pokemonName').value.toLowerCase();
     const apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonName}/`;
 
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            const pokemonDetails = document.getElementById('pokemonDetails');
-            const pokemonNameDetail = document.getElementById('pokemonNameDetail');
-            const pokemonImage = document.getElementById('pokemonImage');
-            const pokemonHeight = document.getElementById('pokemonHeight');
-            const pokemonWeight = document.getElementById('pokemonWeight');
-            const pokemonType = document.getElementById('pokemonType');
-            const pokemonAbilities = document.getElementById('pokemonAbilities');
-            const pokemonMoves = document.getElementById('pokemonMoves');
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
 
-            pokemonNameDetail.textContent = data.name.toUpperCase();
-            pokemonImage.src = data.sprites.front_default;
-            pokemonImage.alt = data.name;
-            pokemonHeight.textContent = `Altura: ${data.height / 10} m`;
-            pokemonWeight.textContent = `Peso: ${data.weight / 10} kg`;
+        const pokemonDetails = document.getElementById('pokemonDetails');
+        const pokemonNameDetail = document.getElementById('pokemonNameDetail');
+        const pokemonImage = document.getElementById('pokemonImage');
+        const pokemonHeight = document.getElementById('pokemonHeight');
+        const pokemonWeight = document.getElementById('pokemonWeight');
+        const pokemonType = document.getElementById('pokemonType');
+        const pokemonAbilities = document.getElementById('pokemonAbilities');
+        const pokemonMoves = document.getElementById('pokemonMoves');
 
-            // Muestra el tipo del Pokémon
-            pokemonType.textContent = `Tipo: ${data.types.map(type => type.type.name).join(', ')}`;
+        pokemonNameDetail.textContent = data.name.toUpperCase();
+        pokemonImage.src = data.sprites.front_default;
+        pokemonImage.alt = data.name;
+        pokemonHeight.textContent = `Altura: ${data.height / 10} m`;
+        pokemonWeight.textContent = `Peso: ${data.weight / 10} kg`;
 
-            // Muestra las habilidades del Pokémon
-            pokemonAbilities.textContent = `Habilidades: ${data.abilities.map(ability => ability.ability.name).join(', ')}`;
+        pokemonType.textContent = `Tipo: ${data.types.map(type => type.type.name).join(', ')}`;
+        pokemonAbilities.textContent = `Habilidades: ${data.abilities.map(ability => ability.ability.name).join(', ')}`;
 
-            // Muestra la lista de movimientos del Pokémon
-            const movesList = data.moves.map(move => move.move.name).slice(0, 5).join(', ');
-            pokemonMoves.textContent = `Movimientos: ${movesList}`;
+        const movesList = data.moves.map(move => move.move.name).slice(0, 5).join(', ');
+        pokemonMoves.textContent = `Movimientos: ${movesList}`;
 
-            pokemonDetails.style.display = 'block';
-        })
-        .catch(error => {
-            console.log(error);
-            alert('Pokémon no encontrado. Intenta de nuevo.');
-        });
+        pokemonDetails.style.display = 'block';
+    } catch (error) {
+        console.log(error);
+        alert('Pokémon no encontrado. Intenta de nuevo.');
+    }
 }
+
+async function loadPokemonList() {
+    const apiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=30';
+
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        const pokemonList = document.getElementById('pokemonList');
+        const pokemonArray = data.results;
+
+        pokemonList.innerHTML = '';
+
+        for (let index = 0; index < pokemonArray.length; index++) {
+            const pokemon = pokemonArray[index];
+            const listItem = document.createElement('li');
+            listItem.textContent = `${index + 1}. ${pokemon.name}`;
+            listItem.addEventListener('click', () => {
+                searchPokemon(pokemon.name);
+            });
+
+            pokemonList.appendChild(listItem);
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+loadPokemonList();
 function clearPokemonDetails() {
     const pokemonDetails = document.getElementById('pokemonDetails');
     const pokemonNameDetail = document.getElementById('pokemonNameDetail');
@@ -57,36 +82,5 @@ function clearPokemonDetails() {
     pokemonAbilities.textContent = '';
     pokemonMoves.textContent = '';
 
-    pokemonDetails.style.display = 'none'; // Oculta el contenedor de detalles
+    pokemonDetails.style.display = 'none';
 }
-function loadPokemonList() {
-    const apiUrl = 'https://pokeapi.co/api/v2/pokemon?limit=30';
-
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            const pokemonList = document.getElementById('pokemonList');
-            const pokemonArray = data.results;
-
-            // Limpia la lista antes de agregar nuevos elementos
-            pokemonList.innerHTML = '';
-
-            pokemonArray.forEach(pokemon => {
-                const listItem = document.createElement('li');
-                listItem.textContent = pokemon.name;
-
-                // Agrega un evento al elemento de la lista para mostrar los detalles del Pokémon
-                listItem.addEventListener('click', () => {
-                    searchPokemon(pokemon.name);
-                });
-
-                pokemonList.appendChild(listItem);
-            });
-        })
-        .catch(error => {
-            console.log(error);
-        });
-}
-
-// Llama a la función para cargar la lista al cargar la página
-loadPokemonList();
